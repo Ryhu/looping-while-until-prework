b2f539330dd07e4f8d1a301c4922d94b7e766025
Ryan Hu <ryanhuuu@gmail.com>
2018-03-17 22:31:47 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,8 +1,9 @@
 def using_until
   levitation_force = 6
   #your code here
-  until levitation_force==10 do
-    levitation_force++
+  until (levitation_force == 10) do
+    puts 
+    levitation_force += 1
   end
   
 end
